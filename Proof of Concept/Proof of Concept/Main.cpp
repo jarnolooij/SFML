5fb765768a61f3e8d2722352d2deeab50799a6db
Jarno Looij <50831250+jarnolooij@users.noreply.github.com>
2021-12-08 11:20:12 +0100

    create window and hello world

--- Proof of Concept/Proof of Concept/Main.cpp
+++ Proof of Concept/Proof of Concept/Main.cpp
@@ -1,5 +1,30 @@
 #include "SFML/Graphics.hpp"
+#include <iostream>
 int main() 
 {
+	//create a window thats called buas intake with the widht and the height.
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Proof of concept", sf::Style::Close | sf::Style::Titlebar);
+
+	//do things when the window is open.
+	while (window.isOpen()) {
+
+		//close window
+		sf::Event evnt;
+		while (window.pollEvent(evnt)) {
+
+			if (evnt.type == evnt.Closed) {
+
+				window.close();
+			}
+		}
+
+		std::cout << "Hello World!";
+
+		//clear the screen
+		window.clear();
+
+		//display objects
+		window.display();
+	}
 	return 0;
 }
